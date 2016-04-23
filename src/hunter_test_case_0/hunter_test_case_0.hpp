b11f8b68b9481380b3c0affa1a5997e76e3c25e2
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-04-23 20:31:29 +0700

    CMake files

---
+++ src/hunter_test_case_0/hunter_test_case_0.hpp
@@ -0,0 +1,12 @@
+#ifndef HUNTER_TEST_CASE_0_HUNTER_TEST_CASE_0_HPP_
+#define HUNTER_TEST_CASE_0_HUNTER_TEST_CASE_0_HPP_
+
+namespace hunter_test_case_0 {
+
+inline const char* info() {
+  return "hunter_test_case_0";
+}
+
+} // namespace hunter_test_case_0 {
+
+#endif // HUNTER_TEST_CASE_0_HUNTER_TEST_CASE_0_HPP_
